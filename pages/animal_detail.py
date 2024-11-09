e917132958fa93b0fc7025b724504292df2f06e7
Dhrishaj Garg <dhrishaj.garg@student.manchester.ac.uk>
2024-11-09 14:56:18 +0000

    add test data

--- pages/animal_detail.py
+++ pages/animal_detail.py
@@ -1,1 +1,16 @@
 # this is where the animal details will be displayed
+import streamlit as st
+
+color = st.select_slider(
+    "Select a color of the rainbow",
+    options=[
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ],
+)
+st.write("My favorite color is", color)
